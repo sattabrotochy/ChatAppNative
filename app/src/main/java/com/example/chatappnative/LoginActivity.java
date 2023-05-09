be0ce405fb63dddc5e0c4c55dc19ce1cc17bdb68
chosotto09@gmail.com <chosotto09@gmail.com>
2023-05-09 23:41:47 +0600

    day7

--- app/src/main/java/com/example/chatappnative/LoginActivity.java
+++ app/src/main/java/com/example/chatappnative/LoginActivity.java
@@ -1,14 +1,94 @@
 package com.example.chatappnative;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText editTexLogEmail,editTextLogPassword;
+    Button logbutton;
+    TextView logsignup;
+
+    FirebaseAuth firebaseAuth;
+
+    String email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+      bingdingView();
+      firebaseAuth=FirebaseAuth.getInstance();
+
+
+        logsignup.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this,RegistrationActivity.class)));
+
+        logbutton.setOnClickListener(view -> {
+            email=editTexLogEmail.getText().toString().trim();
+            password=editTextLogPassword.getText().toString().trim();
+
+            if(email.isEmpty() && password.isEmpty()){
+                errorToast("Please Complete all required field");
+            }else {
+                userLogin(email,password);
+            }
+
+
+        });
+
     }
+
+    private void userLogin(String email,String password) {
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+                    try {
+                        successToast("Login Successfully done");
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        finish();
+                    }catch (Exception e){
+                        errorToast(e.getMessage());
+                    }
+                }else {
+                    errorToast(Objects.requireNonNull(task.getException()).getMessage());
+                }
+            }
+        });
+
+    }
+
+    private void bingdingView() {
+        logsignup=findViewById(R.id.logsignup);
+        logbutton=findViewById(R.id.logbutton);
+        editTexLogEmail=findViewById(R.id.editTexLogEmail);
+        editTextLogPassword=findViewById(R.id.editTextLogPassword);
+    }
+
+    void errorToast(String error){
+        Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+    }
+
+    void successToast(String success){
+        Toast.makeText(getApplicationContext(), success, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/example/chatappnative/RegistrationActivity.java
+++ app/src/main/java/com/example/chatappnative/RegistrationActivity.java
@@ -2,13 +2,109 @@ package com.example.chatappnative;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    CircleImageView profilerg0;
+
+    EditText rgusername,rgemail,rgpassword,rgrepassword;
+    Button signupbutton;
+    TextView loginbut;
+    FirebaseAuth auth;
+    Uri imageURI;
+    String imageuri;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    ProgressDialog progressDialog;
+
+    String name,password,email,rPassword,status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        bindingView();
+
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Establishing The Account");
+        progressDialog.setCancelable(false);
+
+
+
+        loginbut.setOnClickListener(view -> {
+            startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+            finish();
+        });
+
+
+        signupbutton.setOnClickListener(view -> {
+            name=rgusername.getText().toString();
+            password=rgpassword.getText().toString();
+            email=rgemail.getText().toString();
+            rPassword=rgrepassword.getText().toString();
+            status="Hey I'm Using This Application";
+
+            if (TextUtils.isEmpty(name) || TextUtils.isEmpty(email) ||
+                    TextUtils.isEmpty(password) || TextUtils.isEmpty(rPassword)){
+                progressDialog.dismiss();
+                errorToast("Please Enter Valid Information");
+            }else  if (!email.matches(emailPattern)){
+                progressDialog.dismiss();
+               errorToast("Type A Valid Email Here");
+            }else if (password.length()<6){
+                progressDialog.dismiss();
+                errorToast("Password Must Be 6 Characters Or More");
+            }else if (!password.equals(rPassword)){
+                progressDialog.dismiss();
+                errorToast("The Password Doesn't Match");
+            }else {
+
+            }
+
+        });
+
     }
+
+    private void bindingView() {
+        profilerg0=findViewById(R.id.profilerg0);
+        rgusername=findViewById(R.id.rgusername);
+        rgemail=findViewById(R.id.rgemail);
+        rgpassword=findViewById(R.id.rgpassword);
+        rgrepassword=findViewById(R.id.rgrepassword);
+        signupbutton=findViewById(R.id.signupbutton);
+        loginbut=findViewById(R.id.loginbut);
+    }
+
+    void errorToast(String error){
+        Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+    }
+
+    void successToast(String success){
+        Toast.makeText(getApplicationContext(), success, Toast.LENGTH_SHORT).show();
+    }
+
 }
